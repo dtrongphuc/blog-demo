ffc95989462ee970890c5c11e3ee28b132f0fdc9
dtrongphuc <dtrongphucdv@gmail.com>
2020-09-14 14:15:48 +0700

    scroll event with throttle

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import * as _ from 'lodash';
+
 import Nav from './Nav';
 import Banner from './Banner';
 import Post from './Post';
 import './App.scss';
 
-function App() {
-	return (
-		<div className='app'>
-			<Nav />
-			<Banner />
-			<div className='main'>
-				<Post />
-			</div>
-		</div>
-	);
-}
+const handleScroll = (e: any): void => {
+	let navbar = document.querySelector('.navbar');
+	let y = window?.scrollY;
+	if (y === 0) {
+		navbar?.classList.remove('onScroll');
+	} else {
+		navbar?.classList.add('onScroll');
+	}
+};
 
-export default App;
+export default class App extends React.Component {
+	componentDidMount() {
+		window.addEventListener('scroll', _.throttle(handleScroll, 1000), true);
+	}
+
+	render() {
+		return (
+			<div className='app'>
+				<Nav />
+				<Banner />
+				<div className='main'>
+					<Post />
+				</div>
+			</div>
+		);
+	}
+}
