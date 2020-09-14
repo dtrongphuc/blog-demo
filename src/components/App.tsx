193cc6df546bc301868d219a5c1bff505ecb57c1
dtrongphuc <dtrongphucdv@gmail.com>
2020-09-14 09:58:33 +0700

    banner

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './Nav';
+import Banner from './Banner';
+import './App.scss';
+
+function App() {
+	return (
+		<div className='app'>
+			<Nav />
+			<Banner />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Banner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Banner() {
+	return (
+		<div className='banner'>
+			<div className='banner-fullscreen'></div>
+			<span className='banner-text'>React Blog</span>
+		</div>
+	);
+}
