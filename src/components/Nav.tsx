d8a46e140641822aa4c5e6839b948166143ae2c9
dtrongphuc <dtrongphucdv@gmail.com>
2020-09-14 09:19:07 +0700

    create app --template typescript

---
+++ src/components/Nav.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Nav() {
+	return (
+		<div className='navbar'>
+			<div className='navbar-top'>
+				<div className='navbar-left'>
+					<ul className='menu'>
+						<li>
+							<a href=''>React Blog</a>
+						</li>
+						<li>
+							<a href=''>One</a>
+						</li>
+						<li>
+							<a href=''>Two</a>
+						</li>
+						<li>
+							<a href=''>Three</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+}
