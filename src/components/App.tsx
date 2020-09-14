237419dfb31f28de6b359faaa1babc55fe8ccc1f
dtrongphuc <dtrongphucdv@gmail.com>
2020-09-14 10:45:30 +0700

    post

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './Nav';
 import Banner from './Banner';
+import Post from './Post';
 import './App.scss';
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
 		<div className='app'>
 			<Nav />
 			<Banner />
+			<div className='main'>
+				<Post />
+			</div>
 		</div>
 	);
 }
